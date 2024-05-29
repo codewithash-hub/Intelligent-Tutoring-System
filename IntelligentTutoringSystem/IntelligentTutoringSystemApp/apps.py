5a067f0b1862b19c7e5f6b6695211d6635e23e89
codewithash <402308182@my.richfield.ac.za>
2024-05-29 14:49:31 +0200

    initial commit

---
+++ IntelligentTutoringSystem/IntelligentTutoringSystemApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IntelligenttutoringsystemappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'IntelligentTutoringSystemApp'
